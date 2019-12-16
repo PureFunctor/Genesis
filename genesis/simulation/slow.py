dc4168e142774b991535386e0fd852d5557f706a
PureFunctor <purefunctor@gmail.com>
2019-12-16 10:05:06 +0800

    Add comments.

--- genesis/simulation/slow.py
+++ genesis/simulation/slow.py
@@ -2,22 +2,31 @@ import random
 
 
 def simulate(cc, tc):
+    """Takes the total count of cards in a card set and the count of target
+    cards and returns the count of rolls and duplicates after obtaining every
+    target card."""
+
+    # Ensure that tc is less than cc
     tc = tc if cc >= tc else cc
 
+    # Define the sets that hold the cards
     trg_set = set(range(1, tc + 1))
     unowned = set(range(1, cc + 1))
 
     roll_count = 0
     dupe_count = 0
 
+    # Used for the "guaranteed" roll
     generation_power = 0
 
     while unowned.intersection(trg_set):
         roll_count += 1
 
+        # Regular roll
         rolls = 5 if generation_power < 100 else 4
         cards = (random.randint(1, cc) for _ in range(rolls))
 
+        # Guaranteed roll
         if rolls == 4:
             unique = random.choice(list(unowned))
             unowned.remove(unique)
@@ -34,12 +43,17 @@ def simulate(cc, tc):
 
 
 def frequency(data):
+    """Returns the frequency of items in a data tuple."""
     items = sorted(set(data))
     count = (data.count(i) for i in items)
     return dict(zip(items, count))
 
 
 def aggregate(cc, tc, ss):
+    """Given a sample size, call simulate on a given card count and target
+    count, aggregate and split the data for the roll counts and the duplicate
+    counts, compute the mean and the frequency of items."""
+
     aggregate = (simulate(cc, tc) for _ in range(ss))
     rc, dc = zip(*aggregate)
     return {
